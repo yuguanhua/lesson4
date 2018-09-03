e7052adea0880c5848167cbf72e84981c5baa139
yuguanhua <1065824492@qq.com>
2018-09-03 11:03:00 +0800

    update

--- 1.js
+++
@@ -1,5 +0,0 @@
-var b=300;
-var c=400;
-// git push -u 绑定默认分支
-// git push当绑定默认分支后。直接就可以使用push
-
--- 2.js
+++
@@ -1,6 +0,0 @@
-var a=300;
-var c=400;
-var master=3000;
-//这里是master分支git
-var dev2=30000;
-//这里是dev分支git
